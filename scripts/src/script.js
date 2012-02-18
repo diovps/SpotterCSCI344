8081530767f3ecdabf7b170cff341d8cfe665224
Nathan Harris <nrharris@unca.edu>
2012-02-18 16:27:02 -0500

    Taking steps towards key value pairs

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -11,16 +11,27 @@ $(document).ready(function() {
 
 function main() {
     
-    var search_term = "@"+$("#search").val();
+    var search_term = $("#search").val();
     var s  = new Spotter("twitter.search",
 	{q:search_term,period:120,lang:"en"},
 	{buffer:true,bufferTimeOut:750});
 	
     s.register(function(tweets) {
 	var fixedString = "";
-	var fixed = tweets.text.replace(/(@\w*)|(#\w*)|(http.*)/ig,"").match(/\w*/g);
+	var fixed = tweets.text.replace(/(@\w*)|(#\w*)|(http.*)|(\sRT\s)/ig,"").match(/\w*/g);
+	var fixedAssoc = new Array();
+
 	for(var i in fixed){
-		fixedString+=fixed[i]+" ";
+		//fixedString+=fixed[i]+" ";
+		if(fixed[i].toLowerCase() in fixedAssoc){
+		    fixedAssoc[fixed[i].toLowerCase()]+=1;
+		}else{
+		    fixedAssoc[fixed[i].toLowerCase()] = 1;
+		}
+	}
+	fixedAssoc.sort(sort);
+	for(var j in fixedAssoc){
+		fixedString += "Key: "+j + " " + "Value: "+fixedAssoc[j];
 	}
 
 	$("#tweets").append("<p>"+fixedString+"</p>");
@@ -28,4 +39,8 @@ function main() {
     });
 
     s.start();
+}
+
+function sort(a,b){
+	return a-b;
 }
