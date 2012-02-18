86d336ad3a762012dd989b32837e54e17423eedf
Nathan Harris <nrharris@unca.edu>
2012-02-17 23:18:28 -0500

    Finally got regular expressions

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -4,12 +4,12 @@
 
 $(document).ready(function() {
 	$("#search-button").click(function(){
+		$("#Box").fadeOut();
 		main();
 	});
 });
 
 function main() {
-    //your code goes here
     
     var search_term = "@"+$("#search").val();
     var s  = new Spotter("twitter.search",
@@ -17,11 +17,14 @@ function main() {
 	{buffer:true,bufferTimeOut:750});
 	
     s.register(function(tweets) {
-	
-	
-	$("#tweets").append("<p>"+tweets.text.match(/[A-z]*/g)+"</p>");
-		 
-		   
+	var fixedString = "";
+	var fixed = tweets.text.replace(/(@\w*)|(#\w*)|(http.*)/ig,"").match(/\w*/g);
+	for(var i in fixed){
+		fixedString+=fixed[i]+" ";
+	}
+
+	$("#tweets").append("<p>"+fixedString+"</p>");
+	$("#tweets").append("<p>"+tweets.text+"</p>");
     });
 
     s.start();
